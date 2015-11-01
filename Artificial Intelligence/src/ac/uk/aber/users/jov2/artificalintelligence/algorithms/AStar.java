ca80d2c8afad78f103b95d2a8a28490a9ee33df5
Jose Vives <jota_sk@hotmail.com>
2015-11-01 15:03:46 +0000

    An working A* algorithm. But still need an improvemnt

--- Artificial Intelligence/src/ac/uk/aber/users/jov2/artificalintelligence/algorithms/AStar.java
+++ Artificial Intelligence/src/ac/uk/aber/users/jov2/artificalintelligence/algorithms/AStar.java
@@ -1,6 +1,7 @@
 package ac.uk.aber.users.jov2.artificalintelligence.algorithms;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 import ac.uk.aber.users.jov2.artificalintelligence.MyBoard;
 import ac.uk.aber.users.jov2.artificalintelligence.Tile;
@@ -9,72 +10,93 @@ import ac.uk.aber.users.jov2.artificalintelligence.util.Node;
 
 public class AStar extends Algorithm {
 	
+	boolean testB = true;
+
 	private final Heuristic heuristic;
 
 	public AStar(MyBoard myBoard, Tile myTile, Heuristic heuristic) {
 		super(myBoard, myTile);
 		this.heuristic = heuristic;
 	}
-	
+
 	@Override
 	public MyBoard solve(MyBoard mb) {
+		
+		Node start = new Node(mb);
+		start.setG(0);
+		start.setDepth(0);
+		Node goal = new Node(myBoard.getGoalBoard());
+
+		ArrayList<Node> open = new ArrayList<Node>();
+		ArrayList<Node> closed = new ArrayList<Node>();
+		open.add(start);
 		
 		myBoard.stepCounter = -1;
 		myTile.getSoluLabel().setText("Searching ...");
 		boolean displaySearch = myTile.getCBDisplay().getState();
-		
-		ArrayList<Node> open = new ArrayList<Node>();
-		ArrayList<Node> closed = new ArrayList<Node>();
-		
-		Node start = new Node(mb, this.heuristic);
-		start.setG(0);
-		start.setDepth(0);
-		start.knowH();
-		
-		open.add(start);
+
+		int depth = start.getDepth();
 		
 		Node current = null;
-		while(!open.isEmpty() && !myBoard.stopAlgorithm){
+		while (!open.isEmpty() && !myBoard.stopAlgorithm) {
 			current = getLowest(open);
-			if(current.isGoal()){
-				System.out.println("done");
+			if (current.isGoal()) {
 				return finalise(current.getBoard(), displaySearch, current.getDepth());
 			}
+			this.updateGUIWithDetph(displaySearch, current.getBoard(), current.getDepth());
 			open.remove(current);
 			closed.add(current);
-			int depth = current.getDepth() + 1;
-			this.updateGUIWithDetph(displaySearch, current.getBoard(), depth);
+			depth++;
 			ArrayList<Node> succesors = current.getSuccessors(depth);
-			for(Node node: succesors){
+			for (Node node : succesors) {
+				node.setG(current.getG() + 1);
 				node.setParent(current);
-				if(!closed.contains(node)){
-					if(!open.contains(node)){
-						open.add(node);
-					}else{
-						Node openN = open.get(open.indexOf(node));
-						if(node.getG() < openN.getG()){
-							openN.setG(node.getG());
-							openN.setParent(node.getParent());
+				node.setH(this.heuristic.calculate(node, goal));
+				if (!open.contains(node) && !closed.contains(node)) {
+					open.add(node);
+				} else {
+					Node tmp = inList(node, open);
+					if(tmp != null){
+						if(node.getG() < tmp.getG()){
+							open.remove(tmp);
+							open.add(node);
+						}else{
+							tmp = inList(node, closed);
+							if(tmp != null){
+								if(node.getG() < tmp.getG()){
+									closed.remove(tmp);
+									closed.add(node);
+								}
+							}
 						}
 					}
+					
 				}
 			}
 		}
-		System.out.println("path not finded");
+		System.out.println("Not solution founded!");
 		return null;
 	}
 	
-	private Node getLowest(ArrayList<Node> open){
+	private Node inList(Node node, Collection<Node> list){
+		for(Node n: list){
+			if(node.equals(n)){
+				return n;
+			}
+		}
+		return null;
+	}
+
+	private Node getLowest(ArrayList<Node> open) {
 		Node lowest = null;
 		int lowestF = Integer.MAX_VALUE;
-		for(Node n: open){
-			if(n.getF() < lowestF){
+		for (Node n : open) {
+			if (n.getF() < lowestF) {
 				lowest = n;
 				lowestF = lowest.getF();
 			}
 		}
 		return lowest;
 	}
-	
-}
 
+}
