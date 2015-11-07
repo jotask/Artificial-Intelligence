40934a931e3d6728a18c1851df0c29979e348ef6
Jose Vives <jota_sk@hotmail.com>
2015-11-07 16:15:04 +0000

    Implemented two heuristic

---
+++ Artificial Intelligence/src/ac/uk/aber/users/jov2/artificalintelligence/algorithms/heuristics/TwoHeuristics.java
@@ -0,0 +1,22 @@
+package ac.uk.aber.users.jov2.artificalintelligence.algorithms.heuristics;
+
+import ac.uk.aber.users.jov2.artificalintelligence.util.Node;
+
+public class TwoHeuristics extends Heuristic {
+
+	private Heuristic manhattan, hamming;
+	
+	public TwoHeuristics() {
+		manhattan = new Manhattan();
+		hamming = new Hamming();
+	}
+
+	@Override
+	public int calculate(Node a, Node b) {
+		int h = 0;
+		h += manhattan.calculate(a, b);
+		h += hamming.calculate(a, b);
+		return h;
+	}
+
+}
